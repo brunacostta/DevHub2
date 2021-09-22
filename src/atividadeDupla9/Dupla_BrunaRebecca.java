de5554087dde95d8eb3b51707488cb23acbee9ca
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-09-22 14:49:39 -0300

    Atv dupla 9 - cadastro de linguagem usando métodos.

--- src/atividadeDupla9/Dupla_BrunaRebecca.java
+++ src/atividadeDupla9/Dupla_BrunaRebecca.java
@@ -9,7 +9,6 @@ public class Dupla_BrunaRebecca {
     public static void main(String[] args) {
         cabecalho();
         escolha_menu();
-
     }
 
     static void cabecalho(){
@@ -25,6 +24,7 @@ public class Dupla_BrunaRebecca {
                 cadastrar_dev();
                 break;
             case 2:
+                cadastrar_linguagem();
                 break;
             case 3:
                 System.out.println("Aplicação finalizada!");
@@ -42,11 +42,16 @@ public class Dupla_BrunaRebecca {
         return numero;
     }
 
+    static String ler_texto(String mensagem) {
+        System.out.println(mensagem);
+        String texto = sc.nextLine();
+        return texto;
+    }
+
     static void cadastrar_dev() {
         boolean invalido = true;
         do {
-            System.out.println("Digite o nome do desenvolvedor: ");
-            String nome = sc.nextLine();
+            String nome = ler_texto("Digite o nome do desenvolvedor: ");
             if (nome.length() < 3) {
                 System.out.println("O nome deve ter, no minimo, três (3) caracteres. ");
             } else {
@@ -56,8 +61,7 @@ public class Dupla_BrunaRebecca {
 
         invalido = true;
         do {
-            System.out.println("Digite o sobrenome do desenvolvedor ");
-            String sobrenome = sc.nextLine();
+            String sobrenome = ler_texto("Digite o sobrenome do desenvolvedor ");
             if (sobrenome.length() < 3) {
                 System.out.println("O sobrenome deve ter, no minimo, três (3) caracteres. ");
             } else {
@@ -66,21 +70,18 @@ public class Dupla_BrunaRebecca {
         } while (invalido);
 
         invalido = true;
-        while (invalido) {
-            System.out.println("Digite a senioridade do desenvolvedor: ");
-            String senioridade = sc.nextLine();
+        do {
+            String senioridade = ler_texto("Digite a senioridade do desenvolvedor: ");
             if (senioridade.length() < 3) {
                 System.out.println("A senioridade deve ter no mínimo, 3 dígitos.  ");
             } else {
                 invalido = false;
             }
-        }
-        while (invalido) ;
+        } while (invalido);
 
         invalido = true;
         do {
-            System.out.println("Digite a idade do dev: ");
-            int idade = sc.nextInt();
+            int idade = ler_numero("Digite a idade do dev: ");
             if (idade < 1) {
                 System.out.println("Idade invalida. Insira a sua idade novamente. ");
             } else {
@@ -89,5 +90,49 @@ public class Dupla_BrunaRebecca {
         } while (invalido);
 
         System.out.printf("Parabens, o dev foi cadastrado com sucesso!");
+    }
+
+    static void cadastrar_linguagem() {
+        int tipoAplicacao;
+        String nomeLinguagem = "", descricao = "", aplicacao = "";
+
+        System.out.println("Cadastro de Linguagem");
+        do {
+            nomeLinguagem = ler_texto("Nome da linguagem: ");
+
+            if (nomeLinguagem.length() < 3) {
+                System.out.println("Valor inválido. O nome deve ter no mínimo 3 caracteres.  Digite novamente");
+            }
+        } while (nomeLinguagem.length() < 3);
+
+        do {
+            descricao = ler_texto("Descrição da linguagem: ");
+
+            if (descricao.length() < 3) {
+                System.out.println("Valor inválido. A descrição deve ter no mínimo 3 caracteres.  Digite novamente");
+            }
+        } while (descricao.length() < 3);
+
+        do {
+            tipoAplicacao = ler_numero("Informe o tipo da aplicação: 1. Frontend; 2. Backend; 3. Mobile");
+
+            switch (tipoAplicacao) {
+                case 1:
+                    aplicacao = "Frontend";
+                    break;
+                case 2:
+                    aplicacao = "Backend";
+                    break;
+                case 3:
+                    aplicacao = "Mobile";
+                    break;
+                default:
+                    System.out.println("Tipo de aplicação inválida. Digite novamente");
+            }
+        } while (tipoAplicacao < 1 || tipoAplicacao > 3);
+
+        System.out.printf("\nCadastro realizado com sucesso \nO nome  informado foi: %s " +
+                "\nA descrição informada foi: %s" +
+                "\nA aplicação informada foi: %s \n ", nomeLinguagem, descricao, aplicacao);
     }
 }
